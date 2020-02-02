33086c7e71a055e2cf1b7fed660ad0a9e8f7a9c8
Kristhine Fertig <krisfertig@gmail.com>
2020-02-02 11:38:19 -0300

    - Adicionados arquivos de imagens/icones e alterada a configuração do ‘manifest.json’ para permitir o ‘App Install Prompt’.
    - Implementado código para permitir a instalação do app PWA no Desktop e Mobile, assim como apresentar a tela de splash ao iniciar o app: criado botão e script de instalação do app.
    
    - Alterado arquivo serviceWorker, e criado ‘service-worker-custom.js’, que possui código base para funcionamento do service worker, assim para para pré-cachear arquivos chaves (para permitir funcionamento offline). Por enquanto, está cacheando somente arquivo ‘offline.html’.

--- src/index.js
+++ src/index.js
@@ -21,12 +21,18 @@ import ReactDOM from "react-dom";
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
+import * as installScript from './installScript';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+	document.getElementById('root'),
+	() => {
+		console.log('component mounted');
+		installScript.init();
+	});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-serviceWorker.unregister();
-//serviceWorker.register();
+//serviceWorker.unregister();
+serviceWorker.register();
